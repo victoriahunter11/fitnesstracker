d3c27623a8abb8cc95720bd62f3983069d67e8b1
Victoria Hunter <victoria.jane09@gmail.com>
2021-01-06 18:47:38 -0500

    filling out workout.js

--- models/workouts.js
+++ models/workouts.js
@@ -2,10 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  title: String,
-  body: String
+  day: Date,
+  exercises: [
+      {
+      type: String,
+      name: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      duration: Number,
+      distance: Number
+     }]
 });
 
-const Workout = mongoose.model("Workout", WorkoutsSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
